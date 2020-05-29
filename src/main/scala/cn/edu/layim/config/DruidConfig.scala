01f4cfa2670b89c7a9329af374301e1792c7b9e7
梦境迷离 <dreamylost@outlook.com>
2020-05-29 23:24:06 +0800

    fix

--- src/main/scala/cn/edu/layim/config/DruidConfig.scala
+++ src/main/scala/cn/edu/layim/config/DruidConfig.scala
@@ -4,7 +4,7 @@ import com.alibaba.druid.support.http.{ StatViewServlet, WebStatFilter }
 import org.springframework.boot.web.servlet.{ FilterRegistrationBean, ServletRegistrationBean }
 import org.springframework.context.annotation.{ Bean, Configuration }
 
-import scala.collection.JavaConversions
+import scala.collection.JavaConverters._
 
 /**
  * Alibaba Druid数据源配置
@@ -24,9 +24,9 @@ class DruidConfig {
   def statViewServlet(): ServletRegistrationBean = {
     val druid = new ServletRegistrationBean()
     druid.setServlet(new StatViewServlet())
-    druid.setUrlMappings(JavaConversions.asJavaCollection(List("/druid/*")))
+    druid.setUrlMappings(List("/druid/*").asJava)
     val params = Map("loginUsername" -> "admin", "loginPassword" -> "admin", "allo" -> "", "resetEnable" -> "false")
-    druid.setInitParameters(JavaConversions.mapAsJavaMap(params))
+    druid.setInitParameters(params.asJava)
     druid
   }
 
@@ -39,7 +39,7 @@ class DruidConfig {
   def webStatFilter(): FilterRegistrationBean = {
     val filter = new FilterRegistrationBean()
     filter.setFilter(new WebStatFilter())
-    filter.setUrlPatterns(JavaConversions.asJavaCollection(List("/*")))
+    filter.setUrlPatterns(List("/*").asJava)
     filter.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*")
     filter
   }

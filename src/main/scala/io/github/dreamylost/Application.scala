e106a4b32b167d083ecdb49958262c1ea98e5d49
梦境迷离 <dreamylost@outlook.com>
2021-11-29 00:20:10 +0800

    更改conf

--- src/main/scala/io/github/dreamylost/Application.scala
+++ src/main/scala/io/github/dreamylost/Application.scala
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 
 /** IDEA添加 vm参数 -Dspring.output.ansi.enabled=ALWAYS  打印彩色控制台
- */
+  */
 @SpringBootApplication
 @EntityScan(Array("io.github.dreamylost.model"))
 @MapperScan(Array("io.github.dreamylost.repository"))
